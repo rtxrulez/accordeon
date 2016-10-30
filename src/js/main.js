0464d9ab1609bcb235081f104db4cb803d8a9f33
vafarulez <vafarulez@ya.ru>
2016-10-30 13:53:43 +0300

    includes template

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,14 @@
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-  	var item = document.querySelectorAll('.accordeon--parent');
-	item.onclick = function() {
-	    alert( 'Спасибо' );
-	    return false;
+function ready() {
+	var item = document.querySelectorAll('.accordeon--parent');
+
+	for(var i=0; i<item.length; i++) {
+		item[i].addEventListener('click', function(event) {
+			event.preventDefault();
+			console.log('ok');
+		}, false);
 	};
-});
+}
+
+document.addEventListener("DOMContentLoaded", ready);
+
